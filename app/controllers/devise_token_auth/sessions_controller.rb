a103b8887d37a0ccf16285c477d3cc2f89d7a997
Hannah Howard <hannah@hannahhoward.net>
2014-09-14 16:59:13 -0700

    save user token data

--- app/controllers/devise_token_auth/sessions_controller.rb
+++ app/controllers/devise_token_auth/sessions_controller.rb
@@ -9,6 +9,15 @@ module DeviseTokenAuth
     def create
       self.resource = warden.authenticate!(auth_options)
       sign_in(resource_name, resource, :store => false)
+      @user = resource
+      @client_id = SecureRandom.urlsafe_base64(nil, false)
+      @token     = SecureRandom.urlsafe_base64(nil, false)
+
+      @user.tokens[@client_id] = {
+        token: BCrypt::Password.create(@token),
+        expiry: (Time.now + DeviseTokenAuth.token_lifespan).to_i
+      }
+      @user.save
       yield resource if block_given?
       render json: {
         data: resource.as_json(except: [
