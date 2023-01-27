09076b70e17c00d4a4ba443d3508b737466e0857
Andy Wang <wangyaodi@gmail.com>
2023-01-27 09:32:05 +0800

    fixed the hash access error

--- lib/devise_token_auth/models/token_authenticatable.rb
+++ lib/devise_token_auth/models/token_authenticatable.rb
@@ -56,33 +56,37 @@ module Devise
 
         return false unless tokens[client_id]
 
-        token_is_current?(token, client_id) &&
-          token_can_be_reused?(token, client_id)
+        return true if token_is_current?(token, client_id)
+        return true if token_can_be_reused?(token, client_id)
+        # return false if none of the above conditions are met
+        return false
       end
 
       def token_is_current?(token, client_id)
         # ensure that expiry and token are set
-        tokens[client_id][:expiry] &&
-          tokens[client_id][:token] &&
+        client_token_meta = tokens[client_id].with_indifferent_access
+        client_token_meta[:expiry] &&
+          client_token_meta[:token] &&
 
           # ensure that the token was created within the last two weeks
-          DateTime.strptime(tokens[client_id][:expiry].to_s, '%s') > Time.zone.now &&
+          DateTime.strptime(client_token_meta[:expiry].to_s, '%s') > Time.zone.now &&
 
           # ensure that the token is valid
-          BCrypt::Password.new(tokens[client_id][:token]) == token
+          BCrypt::Password.new(client_token_meta[:token]) == token
       end
 
       # allow batch requests to use the previous token
       def token_can_be_reused?(token, client_id)
         # ensure that the last token and its creation time exist
-        tokens[client_id][:updated_at] &&
-          tokens[client_id][:last_token] &&
+        client_token_meta = tokens[client_id].with_indifferent_access
+        client_token_meta[:updated_at] &&
+          client_token_meta[:last_token] &&
 
           # ensure that previous token falls within the batch buffer throttle time of the last request
-          Time.parse(tokens[client_id][:updated_at]) > Time.zone.now - DeviseTokenAuth.batch_request_buffer_throttle &&
+          Time.parse(client_token_meta[:updated_at]) > Time.zone.now - DeviseTokenAuth.batch_request_buffer_throttle &&
 
           # ensure that the token is valid
-          BCrypt::Password.new(tokens[client_id][:last_token]) == token
+          BCrypt::Password.new(client_token_meta[:last_token]) == token
       end
 
       # update user's auth token (should happen on each request)
@@ -93,14 +97,17 @@ module Devise
         token_hash   = BCrypt::Password.create(token)
         expiry       = (Time.zone.now + DeviseTokenAuth.token_lifespan).to_i
 
-        last_token = tokens[client_id][:token] if tokens[client_id] && tokens[client_id][:token]
-
+        if tokens[client_id]
+          client_token_meta = tokens[client_id].with_indifferent_access
+          last_token = client_token_meta[:token] if client_token_meta[:token]
+        end
+        
         tokens[client_id] = {
           token: token_hash,
           expiry: expiry,
           last_token: last_token,
           updated_at: Time.zone.now
-        }
+        }.with_indifferent_access
 
         save!
 
@@ -109,10 +116,10 @@ module Devise
 
       def build_auth_header(token, client_id = 'default')
         client_id ||= 'default'
-
+        client_token_meta = tokens[client_id].with_indifferent_access
         # client may use expiry to prevent validation request if expired
         # must be cast as string or headers will break
-        expiry = tokens[client_id][:expiry].to_s
+        expiry = client_token_meta[:expiry].to_s
 
         {
           'access-token' => token,
@@ -124,14 +131,16 @@ module Devise
       end
 
       def build_auth_url(base_url, args)
+        client_token_meta = tokens[args[:client_id]].with_indifferent_access
         args[:uid]    = uid
-        args[:expiry] = tokens[args[:client_id]][:expiry]
+        args[:expiry] = client_token_meta[:expiry]
 
         generate_url(base_url, args)
       end
 
       def extend_batch_buffer(token, client_id)
-        tokens[client_id][:updated_at] = Time.zone.now
+        client_token_meta = tokens[client_id].with_indifferent_access
+        client_token_meta[:updated_at] = Time.zone.now
         save!
 
         build_auth_header(token, client_id)
